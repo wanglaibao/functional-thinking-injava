c488ef8fd9bfe91ccf10f0b09e954d5d79d2947f
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-08-17 14:20:46 +0800

    update java files info

--- masteringlambdas/src/main/java/com/laibao/masteringlambdas/chapter1/compositebehaviour/PointComparatorTest.java
+++ masteringlambdas/src/main/java/com/laibao/masteringlambdas/chapter1/compositebehaviour/PointComparatorTest.java
@@ -11,16 +11,20 @@ import java.util.List;
  */
 public class PointComparatorTest {
 
-    static List<Point> pointList = Arrays.asList(new Point(1,2),new Point(2,2),new Point(3,4),new Point(5,6),new Point(7,8));
+    private static List<Point> pointList = Arrays.asList(new Point(1,2),new Point(2,2),new Point(3,4),new Point(5,6),new Point(7,8));
 
-    static Comparator<Point> comparatorByX = new Comparator<Point>() {
+    private static Comparator<Point> comparatorByX = new Comparator<Point>() {
         @Override
         public int compare(Point point1, Point point2) {
             return Double.compare(point1.getX(),point2.getX());
         }
     };
 
+    private static Comparator<Point> comparatorByY = (Point point1,Point point2) -> Double.compare(point1.getY(),point2.getY());
+
     public static void main(String[] args) {
         pointList.sort(comparatorByX);
+
+        pointList.sort(comparatorByY);
     }
 }
