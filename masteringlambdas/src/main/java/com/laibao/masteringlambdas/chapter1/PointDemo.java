f9a36132758c43f0e089b39fb552e1362c99ee76
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-08-17 09:54:14 +0800

    add new java file info

---
+++ masteringlambdas/src/main/java/com/laibao/masteringlambdas/chapter1/PointDemo.java
@@ -0,0 +1,40 @@
+package com.laibao.masteringlambdas.chapter1;
+
+import java.awt.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+
+
+/**
+ * @author laibao wang
+ * @date 2018-08-17
+ * @version 1.0
+ */
+public class PointDemo {
+
+    public static void main(String[] args) {
+        List<Point> pointList = Arrays.asList(new Point(1,2),new Point(2,2),new Point(3,4),new Point(5,6),new Point(7,8));
+        // in java 5
+        for (Point point: pointList) {
+            point.translate(1,1);
+        }
+
+        // before java 5
+        for (Iterator iterator = pointList.iterator();iterator.hasNext();) {
+            Point point = (Point) iterator.next();
+            point.translate(1,1);
+        }
+
+        // before java 5
+        Iterator iterator = pointList.iterator();
+        while (iterator.hasNext()) {
+            Point point = (Point) iterator.next();
+            point.translate(1,1);
+        }
+
+
+        // in java 8
+        pointList.forEach(point -> point.translate(1,1));
+    }
+}
