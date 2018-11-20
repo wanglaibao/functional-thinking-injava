d9e2a1049af19d959a8bb8a260019861decf4def
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-21 00:35:40 +0800

    update java file info

--- functionalprogramintroduction/src/main/java/com/laibao/functionintroduction/chapter1/FunctionMain.java
+++ functionalprogramintroduction/src/main/java/com/laibao/functionintroduction/chapter1/FunctionMain.java
@@ -15,9 +15,9 @@ public class FunctionMain {
     private static Function<String,String> function1 = String::toUpperCase;
 
     public static void main(String[] args) {
-        computeOne(100,number -> number + 1000);
-        computeTwo(1000,200,(Integer a,Integer b) -> a + b);
-        convertToString(123123,number -> String.valueOf(number));
+        System.out.println(computeOne(100,number -> number + 1000));
+        System.out.println(computeTwo(1000,200,(Integer a,Integer b) -> a + b));
+        System.out.println(convertToString(123123,number -> String.valueOf(number)));
 
         System.out.println(function.apply("jinge"));
         System.out.println(function1.compose(function).apply("jinge"));
@@ -25,6 +25,9 @@ public class FunctionMain {
 
         Comparator<String> comparator = (String one, String two) -> one.compareTo(two);
         Comparator<String> reversed = comparator.reversed();
+
+        System.out.println(compute(2000, (Integer number) -> number + 2000));
+        System.out.println(compute(2000, (Integer number) -> String.valueOf(number))+"sadfasfd".toUpperCase());
     }
 
     private static Integer computeOne(int number, Function<Integer,Integer> function) {
@@ -37,5 +40,18 @@ public class FunctionMain {
 
     private static String convertToString(int number,Function<Integer, String> function) {
         return function.apply(number);
+    }
+
+
+    /**
+     * 这个是一个通用的抽象 体现了很高的抽象能力
+     * @param t
+     * @param function
+     * @param <T>
+     * @param <R>
+     * @return
+     */
+    private static <T,R> R compute(T t, Function<T,R> function) {
+        return function.apply(t);
     }
 }
