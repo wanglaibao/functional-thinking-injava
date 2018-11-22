f0f3a76411ce6b74e4926d31472567b9dcca8112
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-22 14:20:28 +0800

    update java file info

--- closures_and_lambda/src/main/java/com/laibao/closuresandlambda/understandinglambdas/LambdaExpressionExample.java
+++ closures_and_lambda/src/main/java/com/laibao/closuresandlambda/understandinglambdas/LambdaExpressionExample.java
@@ -1,5 +1,9 @@
 package com.laibao.closuresandlambda.understandinglambdas;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.function.Function;
 
 /**
@@ -11,5 +15,20 @@ public class LambdaExpressionExample {
     //Identity Function Implemented Using a Lambda
     public static <T> Function<T,T> identityFunction() {
         return t -> t;
+    }
+
+    public static void main(String[] args) {
+        Function<File, Byte> firstByte = file -> {
+            try (InputStream is = new FileInputStream(file)) {
+                return (byte) is.read();
+            } catch (IOException ioe) {
+                throw new RuntimeException("Could not read " + file, ioe);
+            }
+        };
+        for (String filename : args) {
+            File file = new File(filename);
+            int byte1 = firstByte.apply(file);
+            System.out.println(filename + "\t=>\t" + byte1);
+        }
     }
 }
