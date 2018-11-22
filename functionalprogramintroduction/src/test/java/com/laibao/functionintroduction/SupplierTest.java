0e573664747e4326ca3631f68ad4096deb4accc1
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-22 09:20:31 +0800

    update java file info

---
+++ functionalprogramintroduction/src/test/java/com/laibao/functionintroduction/SupplierTest.java
@@ -0,0 +1,36 @@
+package com.laibao.functionintroduction;
+
+import com.alibaba.fastjson.JSON;
+import com.laibao.functionintroduction.model.Student;
+import org.junit.Test;
+
+import java.util.function.Supplier;
+
+/**
+ * @author laibao wang
+ */
+public class SupplierTest {
+
+    @Test
+    public void testGetSimpleString() {
+        Supplier<String> supplier = () -> "this is the hello world string";
+        String value = supplier.get();
+        System.out.println("value : "+value);
+    }
+
+    @Test
+    public void testGetStudent() {
+        Supplier<Student> supplier = () -> new Student("jinge",123);
+        Student student = supplier.get();
+        System.out.println(JSON.toJSONString(student));
+    }
+
+    @Test
+    public void testGetStudentTwo() {
+        Supplier<Student> supplier = Student::new; //只能是无惨构造函数，而不能说有参构造函数
+        Student student = supplier.get();
+        student.setName("金戈");
+        student.setAge(100000);
+        System.out.println(JSON.toJSONString(student));
+    }
+}
